3071f837e1a27811df8881cdcf266d210c6bc2d6
EchoKuroneko <138863557+EchoKuroneko@users.noreply.github.com>
2024-09-26 07:16:34 +0300

    add: time in miliseconds

---
+++ src/constants/timeUnits.js
@@ -0,0 +1,9 @@
+const TIME_UNITS = [
+	{ name: "s", value: 1000 },
+	{ name: "m", value: 60000 },
+	{ name: "h", value: 3600000 },
+	{ name: "d", value: 86400000 },
+	{ name: "w", value: 604800000 },
+];
+
+export default TIME_UNITS;
