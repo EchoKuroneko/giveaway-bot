7c254875e29e630546acea6952667b030fbab387
EchoKuroneko <138863557+EchoKuroneko@users.noreply.github.com>
2024-09-26 07:06:04 +0300

    add: scanFolder

---
+++ src/utils/scanFolder.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+import path from "path";
+
+export default function scanFolder(directory, foldersOnly = false) {
+	let fileNames = [];
+	const files = fs.readdirSync(directory, { withFileTypes: true });
+
+	for (const file of files) {
+		const filePath = path.join(directory, file.name);
+		if (foldersOnly) {
+			if (file.isDirectory()) fileNames.push(filePath);
+		} else {
+			if (file.isFile() && file.name.endsWith(".js"))
+				fileNames.push(filePath);
+		}
+	}
+	return fileNames;
+}
