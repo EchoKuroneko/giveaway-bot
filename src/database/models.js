5320ff710e5b2d4aaa391a3d01e3f269b9db7dde
EchoKuroneko <138863557+EchoKuroneko@users.noreply.github.com>
2024-09-26 07:10:30 +0300

    add: participant creation

--- src/database/models.js
+++ src/database/models.js
@@ -8,3 +8,13 @@ export async function createGiveaway(giveawayData) {
 	await gCollection.insertOne(giveawayData);
 	console.log(`Giveaway ${giveawayData.gId} inserted in database!`);
 }
+
+// Participant model functions
+export async function createParticipant(participantData) {
+	const db = await getDatabase(dbName);
+	const pCollection = db.collection("participants");
+	await pCollection.insertOne(participantData);
+	console.log(
+		`Participant ${participantData.uId} for ${participantData.gId} inserted in database!`
+	);
+}
